165130974aba01c354229bbf70f303a101aaa06f
Erick Ruh Cardozo <erickruhcardozo98@gmail.com>
2023-05-12 14:45:13 -0300

    Finalizando Exercicios 11/05

--- Exercicios/Erick/books-array.php
+++ Exercicios/Erick/books-array.php
@@ -7,19 +7,13 @@ $booksCount = isset($_GET['booksCount']) ? $_GET['booksCount'] : 0;
 
 if (isset($_POST['authors'], $_POST['books'], $_POST['pages'])) {
     $authors = $_POST['authors'];
-    $bookNames = $_POST['books'];
+    $books = $_POST['books'];
     $pages = array_map(function($p) { return intval($p); }, $_POST['pages']);
-    $books = [];
 
-    for ($i = 0; $i < count($_GET['authors']); $i++) {
-        $books []= [
-            'author' => $authors[$i],
-            'title' => $bookNames[$i],
-            'pages' => $pages[$i],
-        ];
-    }
-
-    // TODO
+    $maxPages = max($pages);
+    $maxPagesIndex = array_search($maxPages, $pages);
+    $maxPagesBook = $books[$maxPagesIndex];
+    $maxPagesAuthor = $authors[$maxPagesIndex];
 }
 ?>
 <!DOCTYPE html>
@@ -46,27 +40,46 @@ if (isset($_POST['authors'], $_POST['books'], $_POST['pages'])) {
         <h1>Livros</h1>
     </header>
 
-    <form class="d-flex flex-column px-4 gap-3" method="post">
-        <?php for ($i = 1; $i <= $booksCount; $i++) { ?>
-            <fieldset>
-                <legend class="text-center">Livro #<?= $i ?></legend>
-                <div class="d-flex gap-2">
-                    <input class="form-control" type="text" name="authors[]" placeholder="Nome do Autor" required>
-                    <input class="form-control" type="text" name="books[]" placeholder="Nome do Livro" required>
-                    <input class="form-control" type="number" name="pages[]" min="1" placeholder="Quandidade de Páginas" required>
-                </div>
-            </fieldset>
-        <?php } ?>
+    <?php if (isset($maxPages, $maxPagesAuthor, $maxPagesBook)) { ?>
+        <div class="mx-2">
+            <?php for ($i = 0; $i < count($authors); $i++) { ?>
+                <p>Livro: <?= $books[$i] ?></p>
+                <p>Escrito por: <?= $authors[$i] ?></p>
+                <p>Com <?= $pages[$i] ?> páginas</p>
+                <hr>
+            <?php }?>
+            <h2>
+                Livro com mais páginas: <?= $maxPagesBook ?><br>
+                Escrito por: <?= $maxPagesAuthor ?>
+            </h2>
+            <a class="mt-2 btn btn-sm btn-primary" href="<?= $_SERVER['PHP_SELF'] ?>">
+                <i class="bi bi-arrow-left align-text-top"></i>
+                <span>Voltar</span>
+            </a>
+        </div>
+    <?php } else { ?>
+        <form class="d-flex flex-column px-4 gap-3" method="post">
+            <?php for ($i = 1; $i <= $booksCount; $i++) { ?>
+                <fieldset>
+                    <legend class="text-center">Livro #<?= $i ?></legend>
+                    <div class="d-flex gap-2">
+                        <input class="form-control" type="text" name="authors[]" placeholder="Nome do Autor" required>
+                        <input class="form-control" type="text" name="books[]" placeholder="Nome do Livro" required>
+                        <input class="form-control" type="number" name="pages[]" min="1" placeholder="Quandidade de Páginas" required>
+                    </div>
+                </fieldset>
+            <?php } ?>
 
-        <a class="btn btn-primary rounded-circle mt-2 ms-auto" style="width: 42px; height: 42px;" href="?booksCount=<?= $booksCount + 1 ?>">
-            <i class="bi bi-plus-lg"></i>
-        </a>
+            <a class="btn btn-primary rounded-circle mt-2 ms-auto" style="width: 42px; height: 42px;" href="?booksCount=<?= $booksCount + 1 ?>">
+                <i class="bi bi-plus-lg"></i>
+            </a>
 
-        <?php if ($booksCount < 2) { ?>
-            <p class="lead text-center">Adicione pelo menos 2 livros</p>
-        <?php } else { ?>
-            <input class="btn btn-success mt-2" type="submit" value="Cadastrar">
-        <?php } ?>
-    </form>
+            <?php if ($booksCount < 2) { ?>
+                <p class="lead text-center">Adicione pelo menos 2 livros</p>
+            <?php } else { ?>
+                <input class="btn btn-success mt-2" type="submit" value="Cadastrar">
+            <?php } ?>
+        </form>
+    <?php } ?>
 </body>
 </html>
